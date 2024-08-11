e706bb5172052b652b53095d92ad67b558ba4083
alishba-tasleem <alishbatasleem79@gmail.com>
2024-08-12 00:57:58 +0500

    updated app.js file for deployment

--- week-8 project/backend/app.js
+++ week-8 project/backend/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { createRequire } from "module";
 import dotenv from "dotenv";
+import crypto from 'crypto'
+
 import { dbConnection } from "./database/dbConnection.js"
 import { errorMiddleWare } from "./error/error.js"
 import reservationRoute from "./routes/reservationRoute.js"
 
 const app = express();
+// const path = require("path");
+
+const require = createRequire(import.meta.url);
+const path = require('path')
 
 dotenv.config({path: "./config/config.env"});
 
@@ -21,6 +28,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1/reservation", reservationRoute)
 
+app.get("/" , (req, res) => {
+    app.use(express.static(path.resolve(_dirname, "mern frontend", "dist")));
+    res.sendFile(path.resolve(_dirname, "mern frontend", "dist", "index.html"));
+});
 
 dbConnection();
 
